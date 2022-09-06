df306e4d4cde8d186072a549d86beac8e72154cd
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-06 15:17:19 -0300

    feat: implement hidrometer control

---
+++ hidrometer-node/lib/controllers/HidrometerController.ts
@@ -0,0 +1,29 @@
+import { Hidrometer } from '../services/Hidrometer';
+
+export class HidrometerController {
+  private hidrometer: Hidrometer;
+  
+  constructor(hidrometer: Hidrometer){
+    this.hidrometer = hidrometer;
+  }
+
+  public details = async (req: any, res: any) => {
+    return {
+      consumption: this.hidrometer.get_consumption(),
+      flow_rate: this.hidrometer.get_flow_rate(),
+      id: this.hidrometer.get_id()
+    }
+  }
+
+  public pause_flow = async (req: any, res: any): Promise<boolean> => {
+    this.hidrometer.pause_flow();
+    return true;
+  }
+
+
+  public resume_flow = async (req: any, res: any): Promise<boolean> => {
+    this.hidrometer.resume_flow();
+    return true;
+  }
+
+}
