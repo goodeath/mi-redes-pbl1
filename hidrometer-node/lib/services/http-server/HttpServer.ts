2beea6438abf064dce292d9115c65bb0afc5bb30
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-07 07:41:51 -0300

    feat: use our custom http server lib

--- hidrometer-node/lib/services/http-server/HttpServer.ts
+++ hidrometer-node/lib/services/http-server/HttpServer.ts
@@ -1,12 +1,11 @@
-import * as http from 'http';
-
 import { RouteNotFoundException } from "../../exceptions";
 import { REQUEST_METHOD } from "./enums/RequestMethod";
+import { HttpSocket, HttpRequest, HttpResponse } from '../http-socket/';
 
 type RouteList = Record<REQUEST_METHOD, Record<string, Function>>;
 
 export class HttpServer {
-	private server: http.Server | null = null;
+	private server: HttpSocket | null = null;
 
 	private routes: RouteList  = {
 		[REQUEST_METHOD.GET]: {},
@@ -16,34 +15,14 @@ export class HttpServer {
 
 
 	public listen(port: number){
-		this.server = http.createServer( async (req,res) => {
-			const method = <REQUEST_METHOD>req.method?.toLowerCase();
-			let chunks:any[] = [];
-			let data;
-			req.on("data", (chunk) => {
-				chunks.push(chunk);
-			});
-			req.on("end", async () => {
-				data = Buffer.concat(chunks);
-				data = JSON.parse(data.toString());
-				req = <any>{
-					...req,
-					body: data
-				}
-				if(!method || !req.url) {
-					res.end('');
-					return ;
-				}
-				let response = await this.request(method , req.url, req, res);
-				if(typeof response == 'object') response = JSON.stringify(response);
-				res.statusCode = 200
-				res.setHeader('Content-Type', 'text/plain')
-		    	res.end(response)
-			});
-		});
-
-		this.server.listen(port, (): void => {
-    		console.log(`HTTP Server started at port ${port}`);
+		this.server = new HttpSocket();
+		this.server.create_server(port, async (request: HttpRequest, response: HttpResponse) => {
+			const method = <REQUEST_METHOD>request.get_method().toLowerCase();
+			const url = request.get_url();
+			let route_response = await this.request(method , url, request, response);
+			if(typeof route_response == 'object') route_response = JSON.stringify(route_response);
+			response.add_header('Content-Type', 'text/json');
+			response.end(route_response);
 		});
 	}
 
@@ -63,7 +42,7 @@ export class HttpServer {
 		this.set_route(REQUEST_METHOD.POST, route, fn);
 	}
 
-	public async request(method: REQUEST_METHOD, route: string, req:any, res: any): Promise<any | RouteNotFoundException> {
+	public request = async (method: REQUEST_METHOD, route: string, req:any, res: any): Promise<any | RouteNotFoundException> =>  {
 		if(this.routes[method][route])
 			return await this.routes[method][route](req, res);
 		return Promise.reject(new RouteNotFoundException(method, route));
