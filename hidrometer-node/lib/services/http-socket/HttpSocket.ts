2f8b3b1cafd628f4ec0d901686b6ac0ba110fe1d
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-07 07:41:32 -0300

    feat: http server using socket

--- hidrometer-node/lib/services/http-socket/HttpSocket.ts
+++ hidrometer-node/lib/services/http-socket/HttpSocket.ts
@@ -1,5 +1,45 @@
+import * as net from 'net';
 import { HttpRequest } from './HttpRequest';
+import { HttpResponse } from './HttpResponse';
+
 export class HttpSocket {
+	private server: net.Server | null = null;
+
+	public create_server = (port:number , callback: Function): void => {
+		this.server = net.createServer(async (socket: net.Socket) => {
+			let chunks: Buffer[] = []
+			let data;
+			let receiving = true;
+  		  	socket.on('data', (chunk: Buffer) => {
+  		  		receiving = true;
+  		  		chunks.push(chunk);
+			});
+
+			socket.on('end', async () => {
+			});
+			while(receiving){
+				receiving = false;
+				await new Promise((r,j) => setTimeout(r,250));
+			}
+
+			data = Buffer.concat(chunks);
+			data = data.toString();
+			const request = this.parse_request(data);
+			if(request.body)
+				request.body = JSON.parse(request.body);
+			const http_response = new HttpResponse();
+			await callback(request, http_response);
+			
+			socket.write(http_response.get_response());
+			socket.pipe(socket);
+		});
+		this.server.on('error', (err) => {
+  		  console.log(err);
+		});
+		this.server.listen(port, () => {
+  		  console.log(`Server started at ${port}`);
+		});
+	}
 
 	public parse_request = (request: string): HttpRequest => {
 		const pieces = request.split('\n');
