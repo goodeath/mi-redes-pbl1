e45c4235e08c6b1885750a6d1bdae2fa1018118f
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-20 07:56:44 -0300

    feat: export routes and store hidrometer remote addr

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,19 @@
+import * as udp from 'dgram';
 import { SocketUdp } from './lib/services/socket/SocketUdp';
-import { BillRepository, BillHistoryRepository } from './lib/repositories/';
+import { BillRepository, BillHistoryRepository, HidrometerNetinfoRepository } from './lib/repositories/';
 import { Bill } from './lib/models/Bill';
 
 import './config-db.ts';
 
 const socket = new SocketUdp();
-socket.listen(9091, async (data: string) => {
+socket.listen(9091, async (data: string, rinfo: udp.RemoteInfo ) => {
+    const netinfo = new HidrometerNetinfoRepository();
     const bill_repository = new BillRepository();
     const bill_history_repository = new BillHistoryRepository();
     const [register_id, consumption, date] = data.split(',').map( (d:string) => d.trim());
+
+
+    await netinfo.create_or_update(register_id, rinfo.address);
     let current: Bill | undefined = await bill_repository.find_current(register_id);
     if(!current) current = await bill_repository.open(register_id);
     await bill_history_repository.insert(register_id, +consumption);
@@ -23,5 +28,9 @@ const controller = new ServerController();
 const server = new HttpServer();
 
 server.get('/accounts/history', controller.history);
+server.get('/hidrometer/ipaddr', controller.getIpAddr);
 server.get('/accounts/list', controller.list);
+server.post('/accounts/close', controller.close);
+server.post('/accounts/pay', controller.pay);
+
 server.listen(9092);
