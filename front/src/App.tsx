5ef3b9b3dbf1beb5b668cc4a53f4b5fbe63bfa80
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-20 10:48:19 -0300

    refactor: remove unused imports

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { BillApi } from './api/BillApi';
 import { PickMode } from './PickMode';
 import { AdminHome } from './views/admin/AdminHome';
 import { Home } from './views/client/Home';
