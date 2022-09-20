04a1cadf959a3718a335a619ae563e236d5ee043
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-20 07:57:47 -0300

    feat: screen to list user accounts

---
+++ front/src/views/client/AccountList.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BillApi } from '../../api/BillApi';
+
+export const AccountList = () => {
+	const listAccounts = async () => {
+		const api = new BillApi();
+		const r = window.localStorage.getItem('register');
+		const data = await api.list(r);
+		return data;
+	}
+	const [accounts, setAccounts] = useState<any[]>([]);
+	useEffect(() => {
+		listAccounts().then( response => {
+			response.data = response.data.map( data => {
+				data.date_created = new Intl.DateTimeFormat('pt-BR',{year: 'numeric', month: 'numeric', day: 'numeric',
+				  hour: 'numeric', minute: 'numeric', second: 'numeric'}).format(new Date(data.date_created))
+				return data;
+			});
+			setAccounts(response.data);
+		});
+	},[]);
+	return <div>
+		<table>
+			<thead>
+				<tr>
+					<th>Matrícula</th>
+					<th>Data de abertura</th>
+					<th>Status</th>
+					<th>Ação</th>
+				</tr>
+			</thead>
+			<tbody>
+				{accounts.map ((account) => {
+					return <tr>
+						<td>{account.registration_id}</td>
+						<td>{account.date_created}</td>
+						<td>{account.closed ? 'Fechado' : 'Em aberto'}</td>
+						<td><Link to={`/client/account/details/${account.id}`}>Detalhes</Link></td>
+					</tr>
+				})}
+			</tbody>
+		</table>
+	</div>;	
+}
+
