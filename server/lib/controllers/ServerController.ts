f2ffc5e3a2e9110cc320f529574fcf482a93019b
Alex  Morte <alexandersonmbm@gmail.com>
2022-09-20 07:55:10 -0300

    feat: implement close, pay, ipaddr methods

--- server/lib/controllers/ServerController.ts
+++ server/lib/controllers/ServerController.ts
@@ -1,21 +1,44 @@
-import { BillHistoryRepository, BillRepository } from "../repositories";
+import { BillHistoryRepository, BillRepository, HidrometerNetinfoRepository } from "../repositories";
 
 export class ServerController {
   private bill_repository: BillRepository;
+  private hidrometer_netinfo_repository: HidrometerNetinfoRepository;
   private bill_history_repository: BillHistoryRepository;
   
   constructor(){
       this.bill_repository = new BillRepository();
+      this.hidrometer_netinfo_repository = new HidrometerNetinfoRepository();
       this.bill_history_repository = new BillHistoryRepository();
   }
 
   public list = async(req:any, res:any) => {
-      return await this.bill_repository.list();
+      const reg = req.params.r;
+      return await this.bill_repository.list(reg);
   }
 
   public history = async(req:any, res:any) => {
     const {bill_id} = req.params;
-    return await this.bill_history_repository.list(bill_id);
+    return {
+      consumption: await this.bill_history_repository.list(bill_id),
+      bill: await this.bill_repository.findById(bill_id)
+    };
+  }
+
+  public close = async(req:any, res:any) => {
+    const { registration_id } = req.body;
+    return await this.bill_repository.close(registration_id) + "";
+  }
+
+
+  public pay = async(req:any, res:any) => {
+    const { registration_id } = req.body;
+    return await this.bill_repository.pay(registration_id);
+  }
+
+
+  public getIpAddr = async(req:any, res:any) => {
+    const { registration_id } = req.params;
+    return await this.hidrometer_netinfo_repository.find(registration_id);
   }
   
 
