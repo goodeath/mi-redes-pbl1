e926eb54f6ef8622e422e9b2b6e304fb3dbfc1a9
Alex  Morte <alexandersonmbm@gmail.com>
2022-10-31 20:08:12 -0300

    feat: implement mqtt communication

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,28 @@
-import * as udp from 'dgram';
-import { SocketUdp } from './lib/services/socket/SocketUdp';
+import { MqttClient } from './lib/services/mqtt-client/MqttClient';
 import { BillRepository, BillHistoryRepository, HidrometerNetinfoRepository } from './lib/repositories/';
 import { Bill } from './lib/models/Bill';
 
 import './config-db.ts';
 
-// const socket = new SocketUdp();
-// socket.listen(9091, async (data: string, rinfo: udp.RemoteInfo ) => {
-//     const netinfo = new HidrometerNetinfoRepository();
-//     const bill_repository = new BillRepository();
-//     const bill_history_repository = new BillHistoryRepository();
-//     const [register_id, consumption, date, port] = data.split(',').map( (d:string) => d.trim());
+const address = "mqtt://localhost";
+const mqtt = new MqttClient(address, 1883);
+mqtt.subscribe('data');
+mqtt.message(async (topic:any, message:any) => {
+    if(topic == 'data'){
+        const data = message.toString();
+        const netinfo = new HidrometerNetinfoRepository();
+        const bill_repository = new BillRepository();
+        const bill_history_repository = new BillHistoryRepository();
+        const [register_id, consumption, date, port, address] = data.split(',').map( (d:string) => d.trim());
 
 
-//     await netinfo.create_or_update(register_id, rinfo.address+":"+port);
-//     let current: Bill | undefined = await bill_repository.find_current(register_id);
-//     if(!current) current = await bill_repository.open(register_id);
-//     await bill_history_repository.insert(register_id, +consumption);
-// });
-
-// mqtt protocol
-import { MqttClient } from './lib/services/mqtt-client/MqttClient';
-
-const address = "mqtt://broker.emqx.io:1883"
-const ports= 9091
-const mqtt_client = new MqttClient(address,ports);
-
-const topic = ["data"]
-
-mqtt_client.subscribe(topic);
-const data = mqtt_client.message();
-// console.log(data)
-// const [register_id, consumption, date, port] = data.split(',').map( (d:string) => d.trim());
-// console.log(register_id)
+        await netinfo.create_or_update(register_id, address+":"+port);
+        let current: Bill | undefined = await bill_repository.find_current(register_id);
+        if(!current) current = await bill_repository.open(register_id);
+        await bill_history_repository.insert(register_id, +consumption);
+        console.log(message.toString());
+    }
+})
 
 
 // Configure http server
